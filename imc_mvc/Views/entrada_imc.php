b775111dde9f0d63171cc2215626e876d769070e
Vinicius Leonardo Silveira <vinicius.silveira9@fatec.sp.gov.br>
2025-10-28 08:42:09 -0300

    imc mvc

---
+++ imc_mvc/Views/entrada_imc.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>entrada-imc</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div class="card-entrada">
+        <div class="top">
+            <h1>Entrada</h1>
+        </div>
+        <div class="mid">
+            <label for="peso">Peso:</label>
+            <input type="text" name="peso">
+            <label for="peso">Altura:</label>
+            <input type="text" name="altura">
+        </div>
+        <div class="bot">
+            <button>Gerar resultado</button>
+        </div>
+    </div>
+</body>
+
+</html>
